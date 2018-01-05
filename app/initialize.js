5924a17b8359bb254e9713071c11a335a2da74fc
Sukjoon Kim <sjoonk@gmail.com>
2018-01-05 15:20:09 +0900

    initial commit

---
+++ app/initialize.js
@@ -0,0 +1,11 @@
+var Ractive = require('ractive');
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  // do your setup here
+  var ractive = new Ractive({
+    target: '#target',
+    template: '#template'
+  });
+
+});
